6ab83bde61ea301ef0c57f96c2883943ed7212d0
krbu06001 <krbu06001@E-PW05CGQY>
2024-09-12 15:04:38 +0200

    Add project files.

---
+++ Inlamnings uppgift 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Inlamnings_uppgift_2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Windows error");
+        }
+    }
+}
